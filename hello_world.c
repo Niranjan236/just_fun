14cf244ad9a5e0fa635a9feef33fd1ef50d09a0f
Niranjan236 <85889437+Niranjan236@users.noreply.github.com>
2023-10-18 14:22:57 +0530

    delete

--- hello_world.c
+++ hello_world.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
 int main(){
+    char n[10];
     printf("Hello world\n");
     printf("This is my new world");
+    printf("\n Enter something:-");
+    scanf("%s",&n);
+    printf("Delete me");
+    
+
 }
